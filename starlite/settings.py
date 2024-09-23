a01268a001bdea395734a94b08f6452b97713684
Shekinah <MichaelOlatunji.dev@gmail.com>
2024-09-23 14:11:03 +0100

    Create apps

--- starlite/settings.py
+++ starlite/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    # my apps
+    'cart',
+    'order',
+    'product_management',
+    'user_management'
 ]
 
 MIDDLEWARE = [
