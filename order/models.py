dc35e22be9aa8372827746facbd8c2c3ccac8f14
Shekinah <MichaelOlatunji.dev@gmail.com>
2024-09-23 14:45:59 +0100

    Create models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user_management.models import User
+from cart.models import Cart
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    ordered_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, default='pending')
+
+    def __str__(self):
+        return f'Order {self.id} by {self.user.username}'
--- product_management/models.py
+++ product_management/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def _str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    images = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
