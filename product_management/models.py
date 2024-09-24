152249896f92561be3c08d4aaeeb9ddefff998b4
Shekinah <MichaelOlatunji.dev@gmail.com>
2024-09-24 22:31:14 +0100

    Update models

--- product_management/models.py
+++ product_management/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from user_management.models import Seller
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
 
-    def _str__(self):
+    def __str__(self):
         return self.name
     
 
@@ -12,9 +13,17 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
     images = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
+    
+
+class SellerProduct(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} by {self.seller.user.username}'
--- user_management/models.py
+++ user_management/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cart.models import Cart
+from product_management.models import Product
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('buyer', 'Buyer'),
+        ('seller', 'Seller'),
+    )
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=6, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username
+    
+
+class Buyer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    # wishlist = 
+
+    def __str__(self):
+        return f'Buyer: {self.user.username}'
+    
+
+class Seller(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    store_name = models.CharField(max_length=255)
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return f'Seller: {self.user.username}'
