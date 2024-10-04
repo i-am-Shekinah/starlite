24c382cec4d8755669a544eb5fa2af78ab493dd2
Shekinah <MichaelOlatunji.dev@gmail.com>
2024-10-04 21:46:00 +0100

    Add ShippingAddress and PaymentMethod models"

---
+++ shipping_and_delivery_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShippingAndDeliveryManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shipping_and_delivery_management'
---
+++ shipping_and_delivery_management/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class ShippingAddress(models.Model):
+    address_line_1 = models.CharField(max_length=255)
+    address_line_2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.address_line_1}, {self.city}, {self.country}'
+    
+class PaymentMethod(models.Model):
+    PAYMENT_CHOICES = (
+        ('paypal', 'Paypal'),
+        ('credit_card', 'Credit Card'),
+        ('bank_transfer', 'Bank Transfer'),
+    )
+    buyer = models.ForeignKey('user_management.Buyer', on_delete=models.CASCADE, related_name='payment_methods')
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_CHOICES)
+    provider_name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=50)
+    expiry_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.payment_type}, {self.buyer.user.username}'
